8bc08939b80efab8056b2e1725f3d6a944777345
tvoglund <tvoglund@gmail.com>
2015-08-05 23:32:46 -0600

    Added detail to readme.md

---
+++ README.md
@@ -0,0 +1,4 @@
+accounts-saml-shibboleth
+=======
+
+SAML v2 login support for existing password based accounts using Shibboleth IdP
--- package.js
+++ package.js
@@ -1,18 +1,19 @@
 Package.describe({
     name:"tvoglund:accounts-saml-shibboleth",
     summary: "saml login provider for meteor developed for Shibboleth with encrypted Assertion",
-  version: "0.0.1",
-  git: "https://github.com/tvoglund/tvoglund-accounts-saml-shibboleth"
+    version: "0.0.1",
+    git: "https://github.com/tvoglund/tvoglund-accounts-saml-shibboleth",
+    documentation: null
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom("METEOR@1.1.0.2");
+    api.versionsFrom("METEOR@1.1.0.2");
     api.use(['routepolicy','webapp','underscore', 'service-configuration'], 'server');
     api.use(['http','accounts-base'], ['client', 'server']);
 
     api.add_files(['accounts-saml-shibboleth-server.js','accounts-saml-shibboleth-utils.js'], 'server');
 	api.add_files('accounts-saml-shibboleth-client.js', 'client');
-    api.add_files('accounts-saml-shibboleth-log_utils.js', ['client', 'server']);
+    api.add_files('accounts-saml-shibboleth-log-utils.js', ['client', 'server']);
 });
 
 Npm.depends({
